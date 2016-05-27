77c80aeb25c821b9dea8bdd727b46a25fa61b784
Chris Osborn <fozztexx@fozztexx.com>
2016-05-27 12:44:44 -0700

    Actually use the detected parity.

--- src/modem_core.c
+++ src/modem_core.c
@@ -613,7 +613,7 @@ int mdm_handle_char(modem_config *cfg, char ch)
     else if (ch == (char) (cfg->s[SRegisterCR])) {
       // we have a line, process.
       cfg->pchars[2] = ch | parbit;
-      detect_parity(cfg);
+      cfg->parity = detect_parity(cfg);
       cfg->cur_line[cfg->cur_line_idx] = 0;
       strncpy(cfg->last_cmd, cfg->cur_line, sizeof(cfg->last_cmd) - 1);
       mdm_parse_cmd(cfg);
